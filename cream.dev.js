eea640aabddd45fd7a818baaa2898e690789d8a5
Taylor Buley <taylor.buley@gmail.com>
2011-11-07 22:35:04 -0500

    via vim

--- cream.dev.js
+++ cream.dev.js
@@ -371,7 +371,7 @@ var CREAM = ( function () {
 	};
 
 	var filterOutput = function( key, request ) {
-		var timestamp = ( 'undefined' !== typeof request && 'undefined' !== typeof request.timestamp ) ? 0 : parseInt( request.timestamp, 10 )
+		var timestamp = ( 'undefined' !== typeof request && 'undefined' !== typeof request.timestamp ) ? parseInt( request.timestamp, 10 ) : 0
 		    , data = request.data || null
 		    , key = request.key || null;
 
