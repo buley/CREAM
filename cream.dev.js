327059bdedeeb5f1403312cbdd7aabd33c858e76
Taylor Buley <taylor.buley@gmail.com>
2011-11-15 22:53:01 -0500

    adding a method to build the network

--- cream.dev.js
+++ cream.dev.js
@@ -1,8 +1,8 @@
 /* CREAM.dev.js */
 var CREAM = ( function () {
 
-	var user_data_prefix = '$_' //dont use regex
-	  , user_data_prefix_regex = new RegExp( '^' + user_data_prefix.replace( /(\$\^)/g, '\\1' ) )
+	var user_data_prefix = '__' //dont use regex chars (e.g. $ or ^)
+	  , user_data_prefix_regex = new RegExp( '^' + user_data_prefix.replace( /(\$\^)/g, '\\$1' ) )
 	  , cache;
 	if( 'undefined' !== typeof localStorage ) {
 		cache = localStorage.getItem('CREAM');
